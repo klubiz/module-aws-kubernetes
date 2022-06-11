edb33cc2987308c149de565876dd4ef92ef8e14c
cskil <cskil@gmail.com>
2022-06-11 22:20:30 +0900

    kubernetes module complete

---
+++ outputs.tf
@@ -0,0 +1,19 @@
+output "eks_cluster_id" {
+  value = aws_eks_cluster.gitops-cicd-demo.id
+}
+
+output "eks_cluster_name" {
+  value = aws_eks_cluster.gitops-cicd-demo.name
+}
+
+output "eks_cluster_certificate_date" {
+  value = aws_eks_cluster.gitops-cicd-demo.certificate_authority.0.data
+}
+
+output "eks_cluster_endpoint" {
+  value = aws_eks_cluster.gitops-cicd-demo.endpoint
+}
+
+output "eks_cluster_nodegroup_id" {
+  value = aws_eks_node_group.cicd-node-group.id
+}
